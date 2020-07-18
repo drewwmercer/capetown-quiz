a88c898c6faa20d5cbf9ad16205de467eb08772d
Drew Mercer <japanexpress@gmail.com>
2020-07-18 10:39:04 -0400

    Hiding start button if game is active

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,54 @@ import { fetchQuizQuestions } from './API';
 // Components
 import QuestionCard from './components/QuestionCard';
 // Types
-import { Difficulty } from './API';
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
-
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState({});
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
-
   const startQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
 
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
 
-  }
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
-  }
-
-  const nextQuestion = () => {
-
-  }
+  const nextQuestion = () => {};
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Cape Town Quiz</h1>
-      <button className="start" onClick={startQuiz}>
-        Start
-      </button>
-      <p className="score">Score: </p>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startQuiz}>
+          Start
+        </button>
+      ) : null}
+      <p className='score'>Score: </p>
       <p>Loading questions...</p>
       {/* <QuestionCard 
         questionNum={number + 1}
@@ -47,11 +60,11 @@ const App = () => {
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
       /> */}
-      <button className="next" onClick={nextQuestion}>
+      <button className='next' onClick={nextQuestion}>
         Next
       </button>
     </div>
   );
-}
+};
 
 export default App;
