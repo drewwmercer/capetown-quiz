51c7688475aee0db73127e5c0e901ea057d9c22c
Drew Mercer <japanexpress@gmail.com>
2020-07-18 10:26:36 -0400

    Promise is returning full set of answers

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,16 @@
+import { shuffleArray } from './utils';
+import QuestionCard from './components/QuestionCard';
 
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answers: string[];
+    question: string;
+    type: string;
+}
+
+export type QuestionState = Question & { answers: string[] };
 
 export enum Difficulty {
     EASY = 'easy',
@@ -9,5 +21,11 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await fetch(endpoint)).json();
-    console.log(data);
-}
+
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answers: shuffleArray([...question.incorrect_answers, question.correct_answer,]),
+        }
+    ));
+};
